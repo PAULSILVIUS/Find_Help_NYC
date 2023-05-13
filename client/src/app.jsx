f5273bea9b49aa0a0c189b9218415fd90f5f410c
Paul Silvius <paulsilvius0@gmail.com>
2023-05-13 11:47:44 -0400

    frontend outline

---
+++ client/src/app.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import FindTherapist from "./routes/FindTherapist";
+import Update from "./routes/Update";
+
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/therapists/:id/update" component={FindTherapist} />
+          <Route exact path="/therapists/:id" component={Update} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
