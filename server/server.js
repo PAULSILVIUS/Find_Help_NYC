326cd989895c03f5f2fa9e3361609f07e3bf6f37
Paul Silvius <paulsilvius0@gmail.com>
2023-05-12 23:16:25 -0400

    add morgan for middleware

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
-const express = require("express");
 
+const express = require("express");
+// morgan 3rd party middleware
+const morgan = require("morgan");
 const app = express();
 
-// Middleware
+// app.use(morgan("dev"));
+
+// Middleware (Remember to keep on top)
 app.use((req,res,next) => {
     console.log("Our middleware is running");
     next();
